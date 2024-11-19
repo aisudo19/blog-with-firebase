2716758e74137d3418c9e02075ee867ccf0a9383
aisudo19 <indigonaka@gmail.com>
2024-11-19 16:59:44 +0900

    App-routerの追加

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
 import './App.css';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import Home from './components/Home';
+import CreatePost from './components/CreatePost';
+import Login from './components/Login';
+import Logout from './components/Logout';
+import Navbar from './components/Navbar';
 
 function App() {
   return (
-    <div className="App">
-    </div>
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/createpost" element={<CreatePost />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/logout" element={<Logout />} />
+      </Routes>
+    </Router>
   );
 }
 
