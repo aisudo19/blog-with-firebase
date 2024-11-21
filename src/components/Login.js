99875e2db5ece721f75b09033f7c1c64e983673f
aisudo19 <indigonaka@gmail.com>
2024-11-21 16:33:54 +0900

    firebaseのGoogle認証追加、ログイン、ログアウト、ナビゲーション、投稿画面

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,9 +1,21 @@
 import React from 'react'
+import { signInWithPopup } from 'firebase/auth'
+import { auth, provider } from '../firebase';
+import { useNavigate } from 'react-router-dom';
 
-const Login = () => {
+const Login = ({setIsAuth}) => {
+  const navigate = useNavigate();
+  const loginWithGoogle = () => {
+    signInWithPopup(auth, provider).then((result) => {
+      localStorage.setItem('isAuth', true);
+      setIsAuth(true);
+      navigate("/");
+    })
+  }
   return (
     <div>
-      Login
+      <p>ログインする</p>
+      <button onClick={loginWithGoogle}>Googleでログイン</button>
     </div>
   )
 }
